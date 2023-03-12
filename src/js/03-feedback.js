015f9c4e0e2a02a677d907cc378e25b903accf6b
Maria Vovchuk <mari4ka62@gmail.com>
2023-03-12 19:46:59 +0200

    task 3 update2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -14,7 +14,6 @@ const STORAGE_KEY = "feedback-form-state";
 function onFormInput(e) {
     formData.email = email.value;
     formData.message = message.value;
-    console.log(formData);
 
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
@@ -23,6 +22,8 @@ function onFormSubmit(e) {
     e.preventDefault();
     e.currentTarget.reset();
     localStorage.removeItem(STORAGE_KEY);
+
+    console.log(formData);
 }
 
 populateInput();
@@ -37,44 +38,4 @@ function populateInput(){
 }
 
 
-
-
-
-
-
-// import throttle from "lodash.throttle";
-
-// const form = document.querySelector('.feedback-form');
-// const input = document.querySelector('[name="email"]');
-// const textarea = document.querySelector('[name="message"]');
-
-// form.addEventListener('submit', onFormSubmit);
-// form.addEventListener('input', throttle(onFormInput, 500));
-
-// const formData = {};
-
-// const STORAGE_KEY = 'feedback-form-state';
-
-// function onFormInput(event){
-// formData.email = input.value;
-// formData.message = textarea.value;
-
-// localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-// }
-
-// function onFormSubmit(event){
-// event.preventDefault();
-// event.currentTarget.reset();
-// localStorage.removeItem(STORAGE_KEY);
-// }
-
-// reloadForm();
-
-// function reloadForm(event){
-//     const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-//     if(savedData){
-//         input.value = savedData.email;
-//         textarea.value = savedData.message;
-//     }
-// }
-
+ 
