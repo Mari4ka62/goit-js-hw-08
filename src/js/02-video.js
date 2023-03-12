e41903efa2760c3a4cf99eef3330bbd0db23a842
Maria Vovchuk <mari4ka62@gmail.com>
2023-03-12 16:44:24 +0200

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
 
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const iframe = document.querySelector('#vimeo-player');
+    const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onTimeUpdate,1000));
+
+function onTimeUpdate(data) {
+    return localStorage.setItem('videoplayer-current-time',
+    JSON.stringify(data))
+}
+  
+const objectData = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+ 
+player.setCurrentTime(objectData.seconds);
+ 
+ // player.on('play', function () {
+//         console.log('played the video!');
+//     });
+
+//     player.getVideoTitle().then(function(title) {
+//         console.log('title:', title);
+//     });
+//     const onPlay = function(data) {
+     
+// };
+
+// player.on('play', onPlay);
