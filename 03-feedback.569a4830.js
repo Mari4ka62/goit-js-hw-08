bfee9ec72c334d787144d8d47be3c5068ed4c71c
Mari4ka62 <92303981+Mari4ka62@users.noreply.github.com>
2023-03-12 17:12:19 +0000

    Deploying to gh-pages from @ Mari4ka62/goit-js-hw-08@c14b6bec66bc703db0957485ed7fe987c62942d1 🚀

---
+++ 03-feedback.569a4830.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".feedback-form"),t=document.querySelector(".feedback-form input"),n=document.querySelector(".feedback-form textarea");e.addEventListener("input",(function(e){o.email=t.value,o.message=n.value})),e.addEventListener("submit",(function(e){}));const o={};console.log(o);
+//# sourceMappingURL=03-feedback.569a4830.js.map
---
+++ 03-feedback.8ebf473e.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".feedback-form"),t=document.querySelector(".feedback-form input"),n=document.querySelector(".feedback-form textarea");e.addEventListener("input",(function(e){o.email=t.value,o.message=n.value})),e.addEventListener("submit",(function(e){}));var o={};console.log(o)}();
+//# sourceMappingURL=03-feedback.8ebf473e.js.map
