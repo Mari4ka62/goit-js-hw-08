376aa13edf279520cecd94030d23d8d12fe2191c
Maria Vovchuk <mari4ka62@gmail.com>
2023-03-12 19:38:09 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,27 +1,40 @@
+import throttle from "lodash.throttle";
+
 const form = document.querySelector('.feedback-form');
 const email = document.querySelector('.feedback-form input');
 const message = document.querySelector('.feedback-form textarea');
 
 
-form.addEventListener('input', onFormInput);
+form.addEventListener('input', throttle(onFormInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
 const formData = {};
-
+const STORAGE_KEY = "feedback-form-state";
 
 function onFormInput(e) {
     formData.email = email.value;
     formData.message = message.value;
     console.log(formData);
 
-    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function onFormSubmit(e) {
-    
+    e.preventDefault();
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
 }
 
+populateInput();
 
+function populateInput(){
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+    if (savedData) {
+        email.value = savedData.email;
+        message.value = savedData.message;
+    }
+}
 
 
 
